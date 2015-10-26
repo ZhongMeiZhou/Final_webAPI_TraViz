a9ebd167a54d1c168b8b1aea29fe7f440f3faf60
Cesar Ordonez <c_man182@yahoo.com>
2015-10-26 16:04:55 +0800

    implemented vcr for get routes test

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -9,15 +9,30 @@ describe "Check if service is active" do
 end
 
 describe "Check if there is some data" do
-  it "should return tour json" do
-    get '/'
+  it "should return taiwan tour list in json" do
+    VCR.use_cassette('taiwan_tours') do
+      get '/api/v1/taiwan_tours'
+    end
     last_response.must_be :ok?
+    last_response.body.wont_equal ''
   end
 end
 
 describe "Check parameter results" do
   it "should receive parameter and return a json" do
-    get '/'
+    VCR.use_cassette('honduras_tours') do
+      get '/api/v1/tours/honduras.json'
+    end
     last_response.must_be :ok?
+    last_response.body.wont_equal ''
+  end
+end
+
+describe "Check bad parameter results" do
+  it "should return nothing" do
+    VCR.use_cassette('zamunda_tours') do
+      get '/api/v1/tours/zamunda.json'
+    end
+    last_response.body.must_equal ''
   end
 end
