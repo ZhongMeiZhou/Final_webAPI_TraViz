ce4f3b9fd8c8382e46f8eae779017df9d368be6f
nweat <nikkiweat@gmail.com>
2015-10-31 11:05:25 +0800

    Revisions to app helper

---
+++ app_helper.rb
@@ -0,0 +1,11 @@
+require_relative 'model/lonely_planet_tours'
+
+module VisualizerAPIHelpers
+	VERSION = '1.0.0'
+
+	def get_tours(country)
+      Tours.new(country)
+    rescue
+     halt 404
+    end
+end
