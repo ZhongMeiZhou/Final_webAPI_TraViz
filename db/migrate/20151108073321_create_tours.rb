d09eff35386f71da6d74a4684aa395fed368bc08
nweat <nikkiweat@gmail.com>
2015-11-08 17:51:50 +0800

    Updates to DB_migration

--- db/migrate/20151108073321_create_tours.rb
+++ db/migrate/20151108073321_create_tours.rb
@@ -1,11 +1,10 @@
 class CreateTours < ActiveRecord::Migration
   def change
+  	drop_table :tours
   	create_table :tours do |t|
   	  t.string  :country
-  	  t.text :title
-  	  t.float :price
+  	  t.text :tours
+  	  t.timestamps null:false
   	end
   end
 end
-
-# run rake db:migrate
