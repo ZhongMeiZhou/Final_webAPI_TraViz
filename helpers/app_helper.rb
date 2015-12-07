2af01860db0c4785fd4f85d01635c15d4f364ca0
nweat <nikkiweat@gmail.com>
2015-12-07 20:34:17 +0800

    Add helper for tour check

--- helpers/app_helper.rb
+++ helpers/app_helper.rb
@@ -1,6 +1,6 @@
 require_relative '../models/lonely_planet_tours'
 
-module VisualizerAPIHelpers
+module LP_APIHelpers
 	VERSION = '1.0.1'
 
 	def get_tours(country)
@@ -8,5 +8,20 @@ module VisualizerAPIHelpers
     rescue StandardError => e
       logger.info e.message
       halt 404, "#{e.message}"
+    end
+
+    def check_db_tours(resultset, country, tourslist)
+      #if country tour details has not changed then show existing DB results
+      if resultset && resultset.country == country && resultset.tours == tourslist
+        'Record exists'
+      else
+      #if tours has changed but record exists just update the tour details
+      if resultset && resultset.tours != tourslist && resultset.country == country
+      	'Record exists but tour details changed'
+      	else 
+      	# if country not yet exists in the DB, save it
+      	'Country does not exist'
+      end
+      end
     end
 end
