4d4edea48a17ed7ba4d7922ed9e19a4c53020d0c
nweat <nikkiweat@gmail.com>
2015-12-17 20:33:08 +0800

    Price range request check

--- services/tours_comparison.rb
+++ services/tours_comparison.rb
@@ -6,7 +6,7 @@ class CompareTours
   def call (req)
     country_arr = !req['tour_countries'].nil? ? req['tour_countries'] : []
     tour_categories = !req['tour_categories'].nil? ? req['tour_categories'] : []
-    price = !req['inputPriceRange'].nil? ? req['inputPriceRange'].split(",").map(&:to_i) : [0, 999999]
+    price = !req['inputPriceRange'].nil? ? req['inputPriceRange'].split(";").map(&:to_i) : [0, 999999]
     tour_price_min = price[0] #!req['tour_price_min'].nil? ? req['tour_price_min'].to_i : 0
     tour_price_max = price[1] #!req['tour_price_max'].nil? ? req['tour_price_max'].to_i : 999999
     tour_comparison = countries_tours(country_arr, tour_categories, tour_price_min, tour_price_max)
