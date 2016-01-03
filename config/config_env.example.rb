f3919633ab288a0bae3b0211b7c413329e69e3d9
bjsalgado <bayardo_salgado@yahoo.com>
2016-01-03 16:53:18 +0800

    explanation of memcache configuration in config_env example

--- config/config_env.example.rb
+++ config/config_env.example.rb
@@ -5,8 +5,18 @@ end
 
 config_env :development, :test do
   set 'AWS_REGION', '...'
+
+  #memcachier region :europe
+  set 'MEMCACHIER_SERVERS', '...'
+  set 'MEMCACHIER_USERNAME', '...'
+  set 'MEMCACHIER_PASSWORD', '...'
 end
 
 config_env :production do
   set 'AWS_REGION', '...'
+
+  #memcachier region :us
+  set 'MEMCACHIER_SERVERS', '...'
+  set 'MEMCACHIER_USERNAME', '...'
+  set 'MEMCACHIER_PASSWORD', '...'
 end
